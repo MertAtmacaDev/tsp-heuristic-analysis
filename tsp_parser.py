6e16e3073257b8ee732be61761123266657751b1
Mert Atmaca <mertatmaca21@gmail.com>
2026-05-23 17:30:08 +0300

    add distance calculation function

--- tsp_parser.py
+++ tsp_parser.py
@@ -1,10 +1,12 @@
+import numpy
+
 coords = []
 flag = True
 with  open("eil51.tsp") as file:
     for row in file:
         if row.startswith("EOF"):
             break
-        
+
         print(row.strip())
 
         if row.startswith("NODE_COORD_SECTION"):
@@ -24,4 +26,18 @@ with  open("eil51.tsp") as file:
             coords.append((int(pieces[1]), int(pieces[2])))
             
         
-        
+def distance_calc(coords):
+    distance = numpy.zeros((len(coords),len(coords)))
+
+    for i in range(len(coords)):
+
+        for j in range(i+1, len(coords)):
+            x1, y1 = coords[i]
+            x2, y2 = coords[j]
+            euclid_calc = numpy.sqrt((x2 - x1)**2 + (y2-y1)**2)
+            distance[i][j] = euclid_calc
+            distance[j][i] = euclid_calc
+
+    return distance
+
+print(distance_calc(coords)[0][1])
